3afbe2c0f180c5a0a0a71404a4dfa41c2c4f2579
kush-raj-nit061 <kushpvt1@gmail.com>
2023-01-19 16:58:49 +0530

    ToDoApp

---
+++ app/src/main/java/com/example/teslamembersrecord/onDialogCloseListner.java
@@ -0,0 +1,7 @@
+package com.example.teslamembersrecord;
+
+import android.content.DialogInterface;
+
+public interface onDialogCloseListner {
+    void onDialogClose(DialogInterface dialogInterface);
+}
