bcca053f6ba53728e349f69900f82d52cee6453d
kush-raj-nit061 <96692422+kush-raj-nit061@users.noreply.github.com>
2023-01-19 17:40:42 +0530

    Delete app directory

--- app/src/main/java/com/example/teslamembersrecord/onDialogCloseListner.java
+++
@@ -1,7 +0,0 @@
-package com.example.teslamembersrecord;
-
-import android.content.DialogInterface;
-
-public interface onDialogCloseListner {
-    void onDialogClose(DialogInterface dialogInterface);
-}
